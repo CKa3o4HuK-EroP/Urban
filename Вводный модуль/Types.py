9e307de83ee95155cf00f47776deb142139b02ad
Gavin <GavinEA@yandex..ru>
2024-11-16 15:40:12 +0500

    confirmed

---
+++ Вводный модуль/Types.py
@@ -0,0 +1,8 @@
+print('1st program')
+print(f'{(9**0.5)*5}\n')
+print('2nd program')
+print(f'{(9.99 > 9.98) and (1000 != 1000.1)}\n')
+print('3rd program')
+print(f'2 + 2 * 2 = {2 + 2 * 2}\n(2 + 2) * 2 = {(2 + 2) * 2}\n2 + 2 * 2 == (2 + 2) * 2? { 2 + 2 * 2 == (2 + 2) * 2 }')
+print('4th program')
+print(int(10 * float('123.456')) % 10)
