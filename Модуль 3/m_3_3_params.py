b13c40287a71334eb9ed503426a6d14c368f5311
Gavin <GavinEA@yandex..ru>
2024-11-28 10:34:50 +0500

    confirmed

--- Модуль 3/m_3_3_params.py
+++ Модуль 3/m_3_3_params.py
@@ -1,2 +1,10 @@
 def print_params(a=1, b='string', c=True):
-    pass
+    print(a, b, c, sep='')
+
+
+values_list_2 = [54.32, 'Строка']
+
+print_params()
+print_params(*values_list_2, 42)
+print_params(b = 25)
+print_params(c = [1,2,3])
