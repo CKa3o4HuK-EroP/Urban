a70a7e4442b1a7562f51fa3c3402fbd3422ef515
Gavin <GavinEA@yandex..ru>
2024-12-12 10:39:47 +0500

    confirmed

---
+++ Module 5/m_5_2_house_uprgade.py
@@ -0,0 +1,31 @@
+class House:
+    def __init__(self, name, floors):
+        self.name = name
+        self.floors = floors
+
+    def go_to(self, floor):
+        if 1 <= floor <= self.floors:
+            for i in range(1, floor + 1):
+                print(i)
+        else:
+            print("Такого этажа не существует.")
+
+    def __str__(self):
+        return f'Название: {self.name}; Кол-во этажей: {self.floors}'
+
+    def __len__(self):
+        return self.floors
+
+
+h1 = House('ЖК Горский', 18)
+h2 = House('Домик в деревне', 2)
+h1.go_to(5)
+h2.go_to(10)
+
+# __str__
+print(h1)
+print(h2)
+
+# __len__
+print(len(h1))
+print(len(h2))
